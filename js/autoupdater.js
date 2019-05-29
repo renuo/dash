6d1f8465dde409c23179af217d0ccbee7f93c760
Josua Schmid <josua.schmid@renuo.ch>
2019-05-29 14:45:49 +0200

    Use etag for quota

--- js/autoupdater.js
+++ js/autoupdater.js
@@ -1,19 +1,17 @@
-// function getCookieValue(a) {
-//   // https://stackoverflow.com/a/25490531/430418
-//   var b = document.cookie.match('(^|[^;]+)\\s*' + a + '\\s*=\\s*([^;]+)'); return b ? b.pop() : '';
-// }
-
 var repoUrl = 'https://api.github.com/repos/renuo/dash/branches/master';
 var localVersion = window.localStorage.getItem('version');
+var etag = undefined;
 
 document.getElementById('version').innerHTML = "Dashboard Version: " + localVersion;
 
 function checkRemoteVersion(repoUrl, localVersion) {
   var request = new XMLHttpRequest();
   request.open("GET", repoUrl);
+  request.setRequestHeader('If-None-Match', etag);
   request.send();
   request.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
+      etag = request.getResponseHeader('ETag');
       var githubBranch = JSON.parse(this.responseText);
       var remoteVersion = githubBranch.commit.sha;
       if(remoteVersion !== localVersion) {
@@ -25,3 +23,4 @@ function checkRemoteVersion(repoUrl, localVersion) {
 }
 
 setInterval(checkRemoteVersion, 10000, repoUrl, localVersion);
+setInterval(location.reload, 86400000, true);
